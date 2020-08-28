40408223d364b61059ca86de5e0a35ded0d6e41b
denisplay <denisplay12@gmail.com>
2020-08-28 22:33:56 +0300

    added controls
    now you can't just type in whatever you want in the date textbox and crash the program :)

--- Food App/AddProduct.Designer.cs
+++ Food App/AddProduct.Designer.cs
@@ -35,6 +35,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -89,11 +90,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Format : dd/mm/yyyy";
+            // 
             // AddProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(269, 257);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -102,6 +113,7 @@
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "AddProduct";
             this.Text = "AddProduct";
             this.ResumeLayout(false);
@@ -117,5 +129,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Food App/AddProduct.cs
+++ Food App/AddProduct.cs
@@ -29,9 +29,32 @@ namespace Food_App
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.IO.File.AppendAllText(@"C:\FoodManager\FoodDate.txt", Convert.ToDateTime(textBox2.Text) + Environment.NewLine);
-            System.IO.File.AppendAllText(@"C:\FoodManager\FoodName.txt", textBox1.Text + Environment.NewLine);
-            label3.Text = "Registered: " + textBox1.Text + " " + Convert.ToDateTime(textBox2.Text).ToShortDateString();
+            if (DateFromatValid())
+            {
+                DateTime dateTime = GetDateFromatChange();
+                System.IO.File.AppendAllText(@"C:\FoodManager\FoodDate.txt",
+                    dateTime + Environment.NewLine);
+
+                System.IO.File.AppendAllText(@"C:\FoodManager\FoodName.txt",
+                    textBox1.Text + Environment.NewLine);
+
+                label3.Text = "Registered: " + textBox1.Text + " " + textBox2.Text;
+            }
+            else
+                label3.Text = "Failed to register check Date format";
+        }
+        private bool DateFromatValid()
+        {
+            return DateTime.TryParse(textBox2.Text, out DateTime result);
+        }
+        public DateTime GetDateFromatChange()
+        {
+            char separator = Convert.ToChar(textBox2.Text[textBox2.Text.Length - 5]);
+            string[] date = textBox2.Text.Split(separator);
+            DateTime dateTime = new DateTime(Convert.ToInt32(date[2]),
+                Convert.ToInt32(date[1]),
+                Convert.ToInt32(date[0]));
+            return dateTime;
         }
     }
 }
